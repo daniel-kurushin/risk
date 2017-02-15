10603e8b9e00ae1961ac22be0f902a6486140196
Курушин Даниил <daniel.kurushin@gmail.com>
2017-02-15 21:58:16 +0500

    interface changed, test data added

--- interface/parser.py
+++ interface/parser.py
@@ -2,6 +2,57 @@ from tkinter import *
 from tkinter import ttk
 
 class ParserInterface(object):
+	data = {
+		'ALTAI_KR': {
+			"ВРП": 0,
+			"Численность населения": 0,
+			"Объем кредитов": 0,
+			"Просроченная зад-ть": 0,
+			"КО + фил.": 0,
+			"Общий объем П/У": 0,
+			"Остатки бюджета на р/с": 0,
+			"Среднедуш. доходы": 0,
+			"Среднедуш. расходы": 0,
+			"Среднемес. з/п": 0,
+		},
+		'NOV-K': {
+			"ВРП": 0,
+			"Численность населения": 0,
+			"Объем кредитов": 0,
+			"Просроченная зад-ть": 0,
+			"КО + фил.": 0,
+			"Общий объем П/У": 0,
+			"Остатки бюджета на р/с": 0,
+			"Среднедуш. доходы": 0,
+			"Среднедуш. расходы": 0,
+			"Среднемес. з/п": 0,
+		},
+		'TOM_O': {
+			"ВРП": 0,
+			"Численность населения": 0,
+			"Объем кредитов": 0,
+			"Просроченная зад-ть": 0,
+			"КО + фил.": 0,
+			"Общий объем П/У": 0,
+			"Остатки бюджета на р/с": 0,
+			"Среднедуш. доходы": 0,
+			"Среднедуш. расходы": 0,
+			"Среднемес. з/п": 0,
+		},
+		'CHIT_O': {
+			"ВРП": 0,
+			"Численность населения": 0,
+			"Объем кредитов": 0,
+			"Просроченная зад-ть": 0,
+			"КО + фил.": 0,
+			"Общий объем П/У": 0,
+			"Остатки бюджета на р/с": 0,
+			"Среднедуш. доходы": 0,
+			"Среднедуш. расходы": 0,
+			"Среднемес. з/п": 0,
+		},
+	}
+
 	def __init__(self, frame):
 		self.toplevel = frame
 
@@ -44,19 +95,28 @@ class ParserInterface(object):
 	def set_data_frame(self):
 		aframe = Frame(self.toplevel)
 
-		self.table = ttk.Treeview(aframe)
-		hsc = Scrollbar(aframe, orient='hor',  command=self.table.xview)
-		vsc = Scrollbar(aframe, orient='vert', command=self.table.yview)
+		tframe = Frame(aframe)
+		bframe = Frame(aframe)
+
+		tframe.pack(side = TOP, expand = 1, fill = BOTH)
+		bframe.pack(side = BOTTOM, fill = BOTH)
+
+		self.table = ttk.Treeview(tframe)
+		hsc = Scrollbar(tframe, orient='hor',  command=self.table.xview)
+		vsc = Scrollbar(tframe, orient='vert', command=self.table.yview)
 		self.table['yscrollcommand'] = vsc.set
 		self.table['xscrollcommand'] = hsc.set
 
-
+		Button(bframe, text = 'Выгрузка в эксель').pack(side = RIGHT)
+		Button(bframe, text = 'Печать').pack(side = RIGHT)
+		# Выгрузка в эксель (csv)
+		# Печать
 
 		self.table.grid(row=0, column=0, sticky='nsew')
 		vsc.grid(row=0, column=1, sticky='ns')
 		hsc.grid(row=1, column=0, sticky='ew')
-		aframe.rowconfigure(0, weight=1)
-		aframe.columnconfigure(0, weight=1)
+		tframe.rowconfigure(0, weight=1)
+		tframe.columnconfigure(0, weight=1)
 
 		return aframe
 
@@ -72,19 +132,59 @@ class ParserInterface(object):
 
 	def fill_region_data(self):
 		self.table["columns"] = self.get_parameter_list()
+		self.table.heading('#0', text = 'Регион')
+		self.table.column('#0', width = 200)
 		for c in self.table["columns"]:
 			self.table.heading(c, text = c)
+			self.table.column(c, anchor = 'e', minwidth = 50, width = 120)			
 
 		self.table.delete(*self.table.get_children())
 		selection = self.regn_select.curselection()
-		# selection.reverse()
+
 		for i in selection:
 			regn = self.regn_select.get(i)
 			code = self.code_list[regn]
 			values = []
-			# for x in self.table["columns"]:
-			# 	if x == "Количество филиалов": v = self.get_koif_by_month()
+			for x in self.table["columns"]:
+				try:
+					values += [self.data[code][x]]
+				except KeyError:
+					values += [0]
 			self.table.insert("" , END, text = regn, values = values)
+
+	def get_parameter_list(self):
+		return [
+		"ВРП",
+		"Численность населения",
+		"Объем кредитов",
+		"Просроченная зад-ть",
+		"КО + фил.",
+		"Общий объем П/У",
+		"Остатки бюджета на р/с",
+		"Среднедуш. доходы",
+		"Среднедуш. расходы",
+		"Среднемес. з/п",
+		]
+
+	def get_region_list(self):
+		return [
+			[
+			"ALTAI_KR",
+			"Алтайский край"
+			],
+			[
+			"NOV-K",
+			"Новосибирская область"
+			],
+			[
+			"TOM_O",
+			"Томская область"
+			],
+			[
+			"CHIT_O",
+			"Забайкальский край"
+			]
+		]
 
 if __name__ == '__main__':
 	root = Tk()
