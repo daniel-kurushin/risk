22e1601fe27245a8e47ffec1027a154cf240352d
Курушин Даниил <daniel.kurushin@gmail.com>
2017-04-12 12:10:10 +0500

    neuro tests & iface fixes

--- neuron.py
+++ neuron.py
@@ -1,70 +1,80 @@
-from testdata import perseptrondata
+import numpy as np
+
+from testdata import perseptrondata, kohonendata
 
 class Neuron(object):
-  a =  0.02
-  b = -0.04
+	a =  0.02
+	b = -0.04
 
-  def __learn(self,D):
-    """
-    Обучение
-    """
-    w = self.w[:]
-    f = self.__call__
-    for d in D:
-      x = d[0]
-      y = d[1]
-      for j in range(len(x)):
-        self.w[j] += self.a * (y - f(x)) * x[j]
-    return w != self.w
+	def __learn(self,D):
+		"""
+		Обучение
+		"""
+		w = self.w[:]
+		f = self.__call__
+		for d in D:
+			x = d[0]
+			y = d[1]
+			for j in range(len(x)):
+				self.w[j] += self.a * (y - f(x)) * x[j]
+				return w != self.w
 
-  def __sigma_s(self,x):
-    return x
+	def __sigma_s(self,x):
+		return x
 
-  def __sigma_l(self,x):
-    if x < 0:
-      return 0
-    if x >= 0 and x <= 1:
-      return x
-    if x > 1:
-      return 1
+	def __sigma_l(self,x):
+		if x < 0:
+			return 0
+		if x >= 0 and x <= 1:
+			return x
+		if x > 1:
+			return 1
 
-  def __sigma_p(self,x):
-    if x > 0:
-      return 1
-    else:
-      return 0
+	def __sigma_p(self,x):
+		if x > 0:
+			return 1
+		else:
+			return 0
 
-  def __call__(self,x):
-    sigma = self.__sigma_s
-    s = self.b
-    for i in range(len(x)):
-      s += self.w[i] * x[i]
-    # w = self.w[:]
-    # s += (w[0]*x[0]**2 + w[1]*x[1]**2) * (w[4]*(x[0]-w[2])**2 + w[5]*(x[1]-w[3])**2)
-    return sigma(s)
+	def __call__(self,x):
+		sigma = self.__sigma_s
+		s = self.b
+		for i in range(len(x)):
+			s += self.w[i] * x[i]
+			# w = self.w[:]
+			# s += (w[0]*x[0]**2 + w[1]*x[1]**2) * (w[4]*(x[0]-w[2])**2 + w[5]*(x[1]-w[3])**2)
+		return sigma(s)
 
-  def __init__(self,D):
-    """
-    Создание и обучение
-    """
-    self.w = [0]*len(D[0][0])
-    self.c = 0
-    while self.__learn(D):
-      self.c += 1
-      if self.c > 10000: return None
+	def __init__(self,D):
+		"""
+		Создание и обучение
+		"""
+		self.w = [0]*len(D[0][0])
+		self.c = 0
+		while self.__learn(D):
+			self.c += 1
+			if self.c % 100 == 0 : print(self.w)
+			if self.c > 10000: return None
 
 
 def main():
-	D = perseptrondata[:10]
-	for x in D:
-		if x[1] == [1,0,0]: x[1] = 10
-		if x[1] == [0,1,0]: x[1] = 20
-		if x[1] == [0,0,1]: x[1] = 30
+	D = []
+	X = np.array(kohonendata)
+	Y = np.rot90(np.array(perseptrondata))[0]
+	Z = np.array([94,2,76,5,422,177,316,1147,195]) # MAX values
+	X = X / Z
 
+	for i in range(len(X)):
+		x = X[i]
+		y = Y[i]
+		if y == [1,0,0]: y = 1
+		if y == [0,1,0]: y = 2
+		if y == [0,0,1]: y = 3
+		D += [[list(x),y]]
 	f = Neuron(D)
 	print(f.w)
 	for x in D:
-		print(f(x[0]),x[0],x[1])
+		print(round(f(x[0]),2),[round(_,2) for _ in x[0]],x[1])
 
 if __name__ == '__main__':
 	main()
